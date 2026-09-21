a1b711f531f1990163b5cef09ec146bd09b9691d
rumman52 <rummanali658@gmail.com>
2026-09-21 10:27:06 +0600

    Wire agent repository into coworker container

--- services/coworker/container.py
+++ services/coworker/container.py
@@ -8,6 +8,7 @@ from .storage import make_storage
 from .action_repository import ActionRepository
 from .actions import ActionService
 from .google_actions import GoogleActions
+from .agent_repository import AgentRepository
 
 
 @dataclass
@@ -17,10 +18,13 @@ class Container:
     storage: object
     verifier: JwtVerifier
     actions: ActionService | None = None
+    agent: AgentRepository | None = None
 
     def __post_init__(self):
         if self.actions is None:
             self.actions = ActionService(ActionRepository(self.repository.sessions, self.settings), GoogleActions(self.settings))
+        if self.agent is None:
+            self.agent = AgentRepository(self.repository.sessions, self.settings)
 
     @classmethod
     def create(cls, settings: Settings):
