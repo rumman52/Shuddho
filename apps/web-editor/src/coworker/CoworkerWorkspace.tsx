06ae3de81bb324fadc0b1f03b388ce15be3ef56a
rumman52 <rummanali658@gmail.com>
2026-09-23 13:58:09 +0600

    Clear pending UI busy state on account transition

--- apps/web-editor/src/coworker/CoworkerWorkspace.tsx
+++ apps/web-editor/src/coworker/CoworkerWorkspace.tsx
@@ -101,6 +101,7 @@ export default function CoworkerWorkspace({ client, email, signOut }: { client: 
     setNotes("");
     setLanguage("en");
     setCustomLanguage("");
+    setBusy("");
     setError("");
     setNotice("");
     setConnection("");
