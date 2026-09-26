a20acc264ce75760e3d05f956411eefb2013f63a
rumman52 <rummanali658@gmail.com>
2026-09-26 23:07:04 +0600

    Verify PA-06 flag during recovery

--- scripts/cohort_recovery_verification.py
+++ scripts/cohort_recovery_verification.py
@@ -45,6 +45,7 @@ CAPABILITY_ATTRS = {
     "research": "research_services_enabled",
     "actions": "actions_enabled",
     "connector_trust_boundary": "connector_trust_boundary_enabled",
+    "connector_reads": "connector_reads_enabled",
     "action_attachments": "action_attachments_enabled",
     "action_reminders": "action_reminders_enabled",
     "action_recipients": "action_recipients_enabled",
